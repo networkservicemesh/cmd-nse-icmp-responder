727a2fa00a716ba96d38084c1a3d2d61ec485349
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2020-09-07 09:50:02 -0700

    implement icmp responder
    Signed-off-by: Frederick F. Kautz IV <fkautz@alumni.cmu.edu>

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/networkservicemesh/cmd-template
 
 go 1.14
+
+require (
+	github.com/antonfisher/nested-logrus-formatter v1.2.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/networkservicemesh/sdk v0.0.0-20200901150249-cb6febfef3af
+	github.com/pkg/errors v0.9.1
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/pflag v1.0.5
+	github.com/spiffe/go-spiffe/v2 v2.0.0-alpha.4.0.20200528145730-dc11d0c74e85
+	google.golang.org/grpc v1.30.0
+)
--- main.go
+++ main.go
@@ -16,5 +16,123 @@
 
 package main
 
+import (
+	"context"
+	"net"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/networkservicemesh/sdk/pkg/tools/grpcutils"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sirupsen/logrus"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	nested "github.com/antonfisher/nested-logrus-formatter"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/chains/endpoint"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/authorize"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/common/mechanisms/kernel"
+	"github.com/networkservicemesh/sdk/pkg/networkservice/ipam/point2pointipam"
+	"github.com/networkservicemesh/sdk/pkg/tools/debug"
+	"github.com/networkservicemesh/sdk/pkg/tools/log"
+	"github.com/networkservicemesh/sdk/pkg/tools/signalctx"
+	"github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
+)
+
+// config
+type Config struct {
+	Name             string        `default:"icmp-server" desc:"Name of ICMP Server"`
+	BaseDir          string        `default:"./" desc:"base directory" split_words:"true"`
+	ListenOn         url.URL       `default:"unix:///listen.on.socket" desc:"url to listen on" split_words:"true"`
+	ConnectTo        url.URL       `default:"unix:///connect.to.socket" desc:"url to connect to" split_words:"true"`
+	MaxTokenLifetime time.Duration `default:"24h" desc:"maximum lifetime of tokens" split_words:"true"`
+	CidrPrefix       string        `default:"169.254.0.0/16" desc:"CIDR Prefix to assign IPs from"`
+}
+
 func main() {
+	// setup context to catch signals
+	ctx := signalctx.WithSignals(context.Background())
+	ctx, cancel := context.WithCancel(ctx)
+
+	// setup logging
+	logrus.SetFormatter(&nested.Formatter{})
+	logrus.SetLevel(logrus.TraceLevel)
+	ctx = log.WithField(ctx, "cmd", os.Args[0])
+
+	if err := debug.Self(); err != nil {
+		log.Entry(ctx).Infof("%s", err)
+	}
+
+	starttime := time.Now()
+
+	// get config from environment
+	config := &Config{}
+	if err := envconfig.Usage("nsm", config); err != nil {
+		logrus.Fatal(err)
+	}
+	if err := envconfig.Process("nsm", config); err != nil {
+		logrus.Fatalf("error processing config from env: %+v", err)
+	}
+
+	log.Entry(ctx).Infof("Config: %#v", config)
+
+	// get x509 source
+	source, err := workloadapi.NewX509Source(ctx)
+	if err != nil {
+		logrus.Fatalf("error getting x509 source: %+v", err)
+	}
+	svid, err := source.GetX509SVID()
+	if err != nil {
+		logrus.Fatalf("error getting x509 svid: %+v", err)
+	}
+	logrus.Infof("SVID: %q", svid.ID)
+
+	_, ipnet, err := net.ParseCIDR(config.CidrPrefix)
+	if err != nil {
+		log.Entry(ctx).Fatalf("error parsing cidr: %+v", err)
+	}
+
+	prefixes := []*net.IPNet{
+		ipnet,
+	}
+
+	ipamServer := point2pointipam.NewServer(prefixes...)
+
+	// icmp-server network service endpoint
+	endpoint := endpoint.NewServer(
+		ctx,
+		config.Name,
+		authorize.NewServer(),
+		spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime),
+		ipamServer,
+		kernel.NewServer())
+
+	// create grpc server
+	// TODO add serveroptions for tracing
+	server := grpc.NewServer(grpc.Creds(credentials.NewTLS(tlsconfig.MTLSServerConfig(source, source, tlsconfig.AuthorizeAny()))))
+	endpoint.Register(server)
+	srvErrCh := grpcutils.ListenAndServe(ctx, &config.ListenOn, server)
+	exitOnErr(ctx, cancel, srvErrCh)
+	log.Entry(ctx).Infof("Startup completed in %v", time.Since(starttime))
+
+	<-ctx.Done()
+}
+
+func exitOnErr(ctx context.Context, cancel context.CancelFunc, errCh <-chan error) {
+	// If we already have an error, log it and exit
+	select {
+	case err := <-errCh:
+		log.Entry(ctx).Fatal(err)
+	default:
+	}
+	// Otherwise wait for an error in the background to log and cancel
+	go func(ctx context.Context, errCh <-chan error) {
+		err := <-errCh
+		log.Entry(ctx).Error(err)
+		cancel()
+	}(ctx, errCh)
 }
