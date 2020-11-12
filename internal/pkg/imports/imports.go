988ddfb8e093f6e92c756c31430a3f1736e170a5
Denis Tingajkin <denis.tingajkin@xored.com>
2020-11-12 16:47:53 +0700

    fix few issues
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- internal/pkg/imports/imports.go
+++ internal/pkg/imports/imports.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/networkservicemesh/sdk/pkg/tools/spanhelper"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/spiffejwt"
 	_ "github.com/networkservicemesh/sdk/pkg/tools/spire"
+	_ "github.com/pkg/errors"
 	_ "github.com/sirupsen/logrus"
 	_ "github.com/spiffe/go-spiffe/v2/bundle/x509bundle"
 	_ "github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
