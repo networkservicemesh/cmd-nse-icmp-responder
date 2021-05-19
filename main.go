ef738b5283ec2eba683c5f9ece436f0bff906a2c
Danil Uzlov <DanilUzlov@yandex.ru>
2021-05-19 10:07:46 +0700

    use new onidle syntax (disable by zero)
    Signed-off-by: Danil Uzlov <DanilUzlov@yandex.ru>

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ type Config struct {
 	Labels           map[string]string `default:"" desc:"Endpoint labels"`
 	DNSConfigs       dnstools.Decoder  `default:"[]" desc:"DNSConfigs represents array of DNSConfig in json format. See at model definition: https://github.com/networkservicemesh/api/blob/main/pkg/api/networkservice/connectioncontext.pb.go#L426-L435" split_words:"true"`
 	CidrPrefix       string            `default:"169.254.0.0/16" desc:"CIDR Prefix to assign IPs from" split_words:"true"`
-	IdleTimeout      *time.Duration    `default:"" desc:"timeout for automatic shutdown when there were no requests for specified time" split_words:"true"`
+	IdleTimeout      time.Duration    `default:"0" desc:"timeout for automatic shutdown when there were no requests for specified time. Set 0 to disable auto-shutdown." split_words:"true"`
 }
 
 // Process prints and processes env to config
@@ -168,19 +168,12 @@ func main() {
 	// ********************************************************************************
 	log.FromContext(ctx).Infof("executing phase 4: create icmp-server network service endpoint")
 	// ********************************************************************************
-	var idleServer networkservice.NetworkServiceServer
-	if config.IdleTimeout == nil {
-		idleServer = null.NewServer()
-	} else {
-		idleServer = onidle.NewServer(ctx, cancel, onidle.WithTimeout(*config.IdleTimeout))
-	}
-
 	responderEndpoint := endpoint.NewServer(ctx,
 		spiffejwt.TokenGeneratorFunc(source, config.MaxTokenLifetime),
 		endpoint.WithName(config.Name),
 		endpoint.WithAuthorizeServer(authorize.NewServer()),
 		endpoint.WithAdditionalFunctionality(
-			idleServer,
+			onidle.NewServer(ctx, cancel, config.IdleTimeout),
 			point2pointipam.NewServer(ipnet),
 			recvfd.NewServer(),
 			mechanisms.NewServer(map[string]networkservice.NetworkServiceServer{
